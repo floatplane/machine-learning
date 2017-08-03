c96cc11b4b85ab12ef8c21e08ad44528ff0c7dba
Brian Sharon <brian@floatplane.us>
2017-08-03 09:44:39 -0700

    Learning curve

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -21,46 +21,17 @@ m = size(X, 1);
 error_train = zeros(m, 1);
 error_val   = zeros(m, 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return training errors in 
-%               error_train and the cross validation errors in error_val. 
-%               i.e., error_train(i) and 
-%               error_val(i) should give you the errors
-%               obtained after training on i examples.
-%
-% Note: You should evaluate the training error on the first i training
-%       examples (i.e., X(1:i, :) and y(1:i)).
-%
-%       For the cross-validation error, you should instead evaluate on
-%       the _entire_ cross validation set (Xval and yval).
-%
-% Note: If you are using your cost function (linearRegCostFunction)
-%       to compute the training and cross validation error, you should 
-%       call the function with the lambda argument set to 0. 
-%       Do note that you will still need to use lambda when running
-%       the training to obtain the theta parameters.
-%
-% Hint: You can loop over the examples with the following:
-%
-%       for i = 1:m
-%           % Compute train/cross validation errors using training examples 
-%           % X(1:i, :) and y(1:i), storing the result in 
-%           % error_train(i) and error_val(i)
-%           ....
-%           
-%       end
-%
-
-% ---------------------- Sample Solution ----------------------
-
-
-
-
-
-
-
-% -------------------------------------------------------------
-
-% =========================================================================
+for i = 1:m
+  # Calculate theta, given a training set and a passed-in lambda
+  theta = trainLinearReg(X(1:i, :), y(1:i), lambda);
+  # Calculate the training error over the training set we used. Note that
+  # lambda = 0 here.
+  J_train = linearRegCostFunction(X(1:i, :), y(1:i), theta, 0);
+  # Calculate the crass-verification error. Note that
+  # lambda = 0 here as well.
+  J_val = linearRegCostFunction(Xval, yval, theta, 0);
+  error_train(i) = J_train;
+  error_val(i) = J_val;
+end
 
 end
