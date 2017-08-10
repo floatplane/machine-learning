663296c92a893191b412161caf878cc0a0db9476
Brian Sharon <brian@floatplane.us>
2017-08-10 11:37:44 -0700

    Calculate best value of C and sigma for dataset 3

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,34 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+learning = 0;
 
+if learning
+  best_error = realmax();
+  best_C = 0;
+  best_sigma = 0;
+  for C = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    for sigma = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+      printf("Checking C = %f, sigma = %f\n", C, sigma);
+      model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+      predictions = svmPredict(model, Xval);
+      error = mean(double(predictions ~= yval));
+      if error < best_error
+        best_error = error;
+        best_C = C;
+        best_sigma = sigma;
+      end
+    end
+  end
 
+  C = best_C;
+  sigma = best_sigma;
 
-
-
+  printf("Learning complete. C = %f, sigma = %f\n", C, sigma);
+else
+  C = 1;
+  sigma = 0.1;
+end
 
 % =========================================================================
 
